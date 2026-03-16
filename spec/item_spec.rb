f9377097b2c9c9b1a3762d869046a4611eae3141
Alec Krsek <aleckrsek@gmail.com>
2026-03-15 20:35:20 -0700

    Implement item tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
+    let(:item) { Item.new('Coke', 3) }
+    
+    it 'has a name' do
+        expect(item.name).to eq('Coke')
+    end
+    
+    it 'has volume' do
+        expect(item.volume).to eq(3)
+    end
 end
