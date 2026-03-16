728ea0bf22fd39f2008bc5c2da38be7a4ba53209
Alec Krsek <aleckrsek@gmail.com>
2026-03-15 20:40:04 -0700

    Implement tests for water_dispenser

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+    it "initalizes with reservoir" do
+        reservoir = double('reservoir')
+        dispenser = WaterDispenser.new(reservoir)
+        expect(dispenser.reservoir).to eq(reservoir)
+    end
 
+    it "dispenses water from the reservoir" do
+        reservoir = double('reservoir')
+        dispenser = WaterDispenser.new(reservoir)
+        vessel = double('vessel', volume: 100)
+        expect(reservoir).to receive(:drain).with(100)
+        dispenser.dispense(vessel)
+    end
 end
