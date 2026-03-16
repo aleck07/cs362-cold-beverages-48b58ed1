ebd434dcc00331511af1df8eb8bdba2c9468fe92
Alec Krsek <aleckrsek@gmail.com>
2026-03-15 20:44:54 -0700

    Implement water_reservoir tests

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,26 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  it 'has a default capacity of 10' do
+    reservoir = WaterReservoir.new
+    expect(reservoir.capacity).to eq(10)
+  end
+
+  it 'is initially empty' do
+    reservoir = WaterReservoir.new
+    expect(reservoir.empty?).to eq(true)
+  end
+
+  it 'is no longer empty after filling' do
+    reservoir = WaterReservoir.new
+    reservoir.fill
+    expect(reservoir.empty?).to eq(true)
+  end
+
+  it 'can be drained' do
+    reservoir = WaterReservoir.new(10, 10)
+    reservoir.drain(5)
+    expect(reservoir.current_water_volume).to eq(5)
+  end
+
 end
